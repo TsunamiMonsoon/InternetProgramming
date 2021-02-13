ea7776920d769bf508b2fed685c28b0121ae3e77
Heather <55356057+TsunamiMonsoon@users.noreply.github.com>
2021-02-13 16:30:54 -0500

    Add files via upload

---
+++ HTTP_Exercise/server2.py
@@ -0,0 +1,35 @@
+# HTTP server using sockets. This server will send a file
+
+import socket
+from HTTP_Exercise import reader_writer
+import mimetypes
+
+
+def send_file_response(rw, message, content_type='text/plain', status=200, status_remark='OK'):
+    # first line
+    rw.write("HTTP/1.1 {} {}\n".format(status, status_remark))
+    # headers
+    rw.write("content-type: {}\n".format(content_type))
+    rw.write("content-length: {}\n".format(len(message)))
+    # blank line
+    rw.write("\n")
+    # message body
+    rw.socket.sendall(message)
+
+
+if __name__ == "__main__":
+    listener = socket.socket()
+    listener.bind(("", 8080))
+    listener.listen(0)
+
+    while True:
+        (sock, addr) = listener.accept()
+        rw = reader_writer(sock)
+        filename = "Webpage.html"
+        with open(filename, 'rb') as file:
+            data = file.read()
+        mtype = mimetypes.guess_type(filename)
+        print(mtype)
+        send_file_response(rw, data, content_type=mtype)
+
+        sock.close()
